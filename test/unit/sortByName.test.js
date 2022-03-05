9a79da58a9463bf62f9b014a6cb1201dcd5aa081
Sergey <tel.cromax@gmail.com>
2022-03-06 10:33:21 +1100

    initial commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,19 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+        "Гарри Поттер",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ]);
+  });
+});
